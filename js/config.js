dcc7efa588ef229e41b1a65cb7f6164c316185f6
redoun-hossain <bd.myandme@gmail.com>
2026-08-02 21:14:52 +0600

    hi

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-  WEBHOOK_TEST_URL: 'https://arfinsami178.app.n8n.cloud/webhook-test/new-customer',
-  WEBHOOK_PROD_URL: 'https://arfinsami178.app.n8n.cloud/webhook/new-customer',
+  WEBHOOK_TEST_URL: 'https://n8n.srv1106977.hstgr.cloud/webhook-test/new-customer',
+  WEBHOOK_PROD_URL: 'https://n8n.srv1106977.hstgr.cloud/webhook/new-customer',
   USE_TEST: true,
   VERSION: 'Version-3.2',
   get WEBHOOK_URL() {
